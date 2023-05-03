53a70ae32d488fb2572c78771ee4788b22c1a654
Syed Zainul Abidin <73888063+sza619@users.noreply.github.com>
2023-05-03 17:45:09 +0530

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>
+        © Copyright sza619. All rights reserved.
+      </small>
+    </footer>
+  );
+}

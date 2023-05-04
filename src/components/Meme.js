d79c8452ec2e30fc5e116f00b54be7bb677f0394
Syed Zainul Abidin <73888063+sza619@users.noreply.github.com>
2023-05-04 12:18:47 +0530

    Update Meme.js

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -84,7 +84,7 @@ export default function Meme() {
             </div>
 
             <div className="meme" id="image-download">
-                <img src={meme.randomImage} className="meme--image" alt="Meme-Image"/>
+                <img src={meme.randomImage} className="meme--image" alt="Could not find"/>
                 <h2 className="meme--text top">{meme.topText}</h2>
                 <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
